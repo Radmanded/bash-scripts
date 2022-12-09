950950e493e75ac8e882a4349f1ba5f858442740
Ruben Dedman <rubend@synnex.com>
2022-12-09 16:59:29 -0600

    changes to script

--- automate-py-virtualenv.sh
+++ automate-py-virtualenv.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Ask user for virtual environment name
-read -p "Enter a name for the virtual environment: " env_name
+#read -p "Enter a name for the virtual environment: " env_name
 
 # Create the virtual environment with the specified name
-python3 -m venv $env_name
+python3 -m venv venv
 
 # Start the virtual environment
-source $env_name/bin/activate
+source venv/bin/activate
 
 # Update pip
 pip install --upgrade pip
